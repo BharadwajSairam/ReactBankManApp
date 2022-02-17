dc21b08628fec9a301e5c6d100dedc35fda5bc88
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2022-02-17 23:22:19 +0000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  
   app.use(
     '/api',
     createProxyMiddleware({
@@ -8,4 +9,5 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
+  
 };
