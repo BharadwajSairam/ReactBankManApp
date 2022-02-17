3b41418614539aae3a3e3ffbc9787440b7387002
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2022-02-17 23:18:05 +0000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://bank2070-env-1.eba-29dnbjtn.us-east-1.elasticbeanstalk.com/',
+      target: 'http://sairam-manchella.us-east-1.elasticbeanstalk.com/',
       changeOrigin: true,
     })
   );
